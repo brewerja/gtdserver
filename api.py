7c84dca6f912cd728d32180d1e8988c091f20694
John Brewer <brewerja@gmail.com>
2012-04-05 16:25:25 -0400

    Flattened the GtdResource, moving all Foreign key names out.

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ from tastypie.contrib.gis.resources import ModelResource
 from tastypie.constants import ALL, ALL_WITH_RELATIONS
 from tastypie.cache import SimpleCache
 from gtdserver.models import *
+import tastypie.resources
 
 
 class DBSourceResource(ModelResource):
@@ -94,8 +95,8 @@ class HostageOutcomeResource(ModelResource):
 
 
 class GtdResource(ModelResource):
-    region = fields.ForeignKey(RegionResource, 'region', full=True)
-    country = fields.ForeignKey(CountryResource, 'country', full=True)
+    region = fields.ForeignKey(RegionResource, 'region')
+    country = fields.ForeignKey(CountryResource, 'country')
     alternative = fields.ForeignKey(AlternativeResource, 'alternative',
                                     null=True)
     attacktype1 = fields.ForeignKey(AttackTypeResource, 'attacktype1',
@@ -110,17 +111,17 @@ class GtdResource(ModelResource):
     claimmode = fields.ForeignKey(ClaimModeResource, 'claimmode', null=True)
     claimmode2 = fields.ForeignKey(ClaimModeResource, 'claimmode2', null=True)
     claimmode3 = fields.ForeignKey(ClaimModeResource, 'claimmode3', null=True)
-    weapontype1 = fields.ForeignKey(WeaponTypeResource, 'weaptype1', null=True)
-    weapontype2 = fields.ForeignKey(WeaponTypeResource, 'weaptype2', null=True)
-    weapontype3 = fields.ForeignKey(WeaponTypeResource, 'weaptype3', null=True)
-    weapontype4 = fields.ForeignKey(WeaponTypeResource, 'weaptype4', null=True)
-    weaponsubtype1 = fields.ForeignKey(WeaponSubtypeResource, 'weapsubtype1',
+    weaptype1 = fields.ForeignKey(WeaponTypeResource, 'weaptype1', null=True)
+    weaptype2 = fields.ForeignKey(WeaponTypeResource, 'weaptype2', null=True)
+    weaptype3 = fields.ForeignKey(WeaponTypeResource, 'weaptype3', null=True)
+    weaptype4 = fields.ForeignKey(WeaponTypeResource, 'weaptype4', null=True)
+    weapsubtype1 = fields.ForeignKey(WeaponSubtypeResource, 'weapsubtype1',
                                        null=True)
-    weaponsubtype2 = fields.ForeignKey(WeaponSubtypeResource, 'weapsubtype2',
+    weapsubtype2 = fields.ForeignKey(WeaponSubtypeResource, 'weapsubtype2',
                                        null=True)
-    weaponsubtype3 = fields.ForeignKey(WeaponSubtypeResource, 'weapsubtype3',
+    weapsubtype3 = fields.ForeignKey(WeaponSubtypeResource, 'weapsubtype3',
                                        null=True)
-    weaponsubtype4 = fields.ForeignKey(WeaponSubtypeResource, 'weapsubtype4',
+    weapsubtype4 = fields.ForeignKey(WeaponSubtypeResource, 'weapsubtype4',
                                        null=True)
     propextent = fields.ForeignKey(PropExtentResource, 'propextent', null=True)
     hostkidoutcome = fields.ForeignKey(HostageOutcomeResource,
@@ -211,3 +212,54 @@ class GtdResource(ModelResource):
         resource_name = 'attacks'
         cache = SimpleCache()
 
+    def dehydrate(self, bundle):
+        """Reference all foreign keys' names."""
+        data = bundle.data
+        obj = bundle.obj
+        data['geog'] = [obj.geog.x, obj.geog.y]
+        data['geom'] = [obj.geom.x, obj.geom.y]
+        data['region'] = obj.region.name
+        data['country'] = obj.country.name
+        if obj.alternative:
+            data['alternative'] = obj.alternative.name
+        if obj.attacktype1:
+            data['attacktype1'] = obj.attacktype1.name
+        if obj.attacktype2:
+            data['attacktype2'] = obj.attacktype1.name
+        if obj.attacktype3:
+            data['attacktype3'] = obj.attacktype1.name
+        if obj.targtype1:
+            data['targtype1'] = obj.targtype1.name
+        if obj.targtype2:
+            data['targtype2'] = obj.targtype2.name
+        if obj.targtype3:
+            data['targtype3'] = obj.targtype3.name
+        if obj.claimmode:
+            data['claimmode'] = obj.claimmode.name
+        if obj.claimmode2:
+            data['claimmode2'] = obj.claimmode2.name
+        if obj.claimmode3:
+            data['claimmode3'] = obj.claimmode3.name
+        if obj.weaptype1:
+            data['weaptype1'] = obj.weaptype1.name
+        if obj.weaptype2:
+            data['weaptype2'] = obj.weaptype2.name
+        if obj.weaptype3:
+            data['weaptype3'] = obj.weaptype3.name
+        if obj.weaptype4:
+            data['weaptype4'] = obj.weaptype4.name
+        if obj.weapsubtype1:
+            data['weapsubtype1'] = obj.weapsubtype1.name
+        if obj.weapsubtype2:
+            data['weapsubtype2'] = obj.weapsubtype2.name
+        if obj.weapsubtype3:
+            data['weapsubtype3'] = obj.weapsubtype3.name
+        if obj.weapsubtype4:
+            data['weapsubtype4'] = obj.weapsubtype4.name
+        if obj.propextent:
+            data['propextent'] = obj.propextent.name
+        if obj.hostkidoutcome:
+            data['hostkidoutcome'] = obj.hostkidoutcome.name
+        if obj.dbsource:
+            data['dbsource'] = obj.dbsource.name
+        return bundle
